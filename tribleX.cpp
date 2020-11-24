413369f8ab28a41190e07086f7409fb1c6d64999
Kerim <kutukmu@gmail.com>
2020-11-24 07:45:35 -0600

    self doc code

--- tribleX.cpp
+++ tribleX.cpp
@@ -6,5 +6,8 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room " << std::endl;
     std::cout << "You need to enter the correct code to continue... " << std::endl;
 
+    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "The codes add up to: " << std::endl;
+
     return 0;
 }
